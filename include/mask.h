189d92f6c1c35789b7b203767747a93f24b6d128
Qian Chen <chenqianfzh@yahoo.com>
2026-05-22 19:09:31 -0700

    delete unused MoE kernel prototype and build artifacts

--- include/mask.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-// Stub: mixtureExpertKernel.cu does not use any mask API.
-// Prevents conflicting utils.h copies from being pulled in.
--- include/softmax.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-// Stub: mixtureExpertKernel.cu includes this but does not use any softmax API.
-// The real softmax.h is not needed; this stub prevents pulling in conflicting utils.h copies.
